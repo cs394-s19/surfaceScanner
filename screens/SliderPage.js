af858549a4dd8152fd6e22f7998471b03240aa00
deokfilho <deokcelmofilho@gmail.com>
2019-05-13 14:05:28 -0500

    navigation still works, created bottom filter bar in SliderPage, with working slider and successfully make an icon appear

--- screens/SliderPage.js
+++ screens/SliderPage.js
@@ -3,27 +3,62 @@ import {
     Slider,
     StyleSheet
 } from 'react-native';
-import { Container, Header, Content, Button, Text, View } from 'native-base';
+import { Container, View } from 'native-base';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+const SliderFunc = ()=> ( <Slider
+    style={{width: 200, height: 40}}
+    minimumValue={0}
+    maximumValue={1}
+    minimumTrackTintColor="#FFFFFF"
+    maximumTrackTintColor="#000000"
+/>);
 
 export default class SliderPage extends React.Component {
-
     render() {
         return (
-            <View>
-                <Container>
-                <Slider
-                    style={{width: 200, height: 40}}
-                    minimumValue={0}
-                    maximumValue={1}
-                    minimumTrackTintColor="#FFFFFF"
-                    maximumTrackTintColor="#000000"
-                />
-                </Container>
+            <View style={styles.container}>
+                <View style={styles.filters}>
+                    <Icon
+                        name="ios-add"
+                        color="#ccc"
+                        size={25}
+                    />
+                </View>
+                <View style={styles.bottom}>
+                    <Slider
+                        style={{width: 200, height: 40}}
+                        minimumValue={0}
+                        maximumValue={1}
+                        minimumTrackTintColor="#FFFFFF"
+                        maximumTrackTintColor="#000000"
+                    />
+                </View>
             </View>
         )
     }
 }
 
-// const styles = StyleSheet.create({
-//
-// });
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'flex-end',
+    },
+    bottom: {
+        backgroundColor: "#a9a9a9",
+        height: '20%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    filters: {
+        backgroundColor: "#808080",
+        alignItems: 'center',
+        height: '10%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    }
+});
